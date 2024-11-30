956fe1d365d1b2cffc8f6ff8be460995dede2a1e
wangziyuan176 <wesayok@outlook.com>
2024-11-30 18:47:16 +0800

    Add files via upload

---
+++ mihota05/st.py
@@ -0,0 +1,10 @@
+import easygui
+from window import window
+
+a=easygui.choicebox('设置','mihota',['时间','关于','返回'])
+if a=='时间':
+    window.py_run('st1.py')
+elif a=='关于':
+    window.py_run('st2.py')
+elif a=='返回':
+    window.py_run('mihota05.py')
